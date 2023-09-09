6c5bf9051d89138678673c543ac2abfffc148967
vivans720 <vivans720@gmail.com>
2023-09-09 17:36:29 +0530

    conversion of datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,19 @@
+// let score = 72
+// let score = "72"
+// let score = "72vs"
+// let score = null
+// let score = undefined
+// let score = "vivan"
+let score = true;
+
+// console.log(typeof score);
+
+let convertToNumber = Number(score);
+
+// console.log(typeof convertToNumber);
+// console.log(convertToNumber);
+
+// let isLoggedIn = 0
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
