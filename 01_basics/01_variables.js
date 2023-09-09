9dbd43d5552960e7f8e59d810b0a83f1d5600e44
vivans720 <vivans720@gmail.com>
2023-09-09 15:36:25 +0530

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144567;
+let accountEmail = "vivans890@gmail.com";
+var accountPassword = "vivan890"
+let accountState;
+
+accountCity = "Delhi";
+
+// accountId = 2; // not allowed
+
+// console.log(accountID);
+
+accountEmail = "svivan780@gmail.com";
+accountPassword = "vivans743";
+accountCity = "Gurgaon";
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+/*
+    Prefer not to use var to declare variables because of issue in block scope and functional scope
+*/
