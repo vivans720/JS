49e8f018b551475a06157addcdb8be5367d0540c
vivans720 <vivans720@gmail.com>
2023-09-09 16:50:48 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict" // treat all JS code as newer version or newer JS
+
+//alert(3+3) will only work in browser console, not in node environment
+
+console.log(3+3)
+
+// avoid using semi colons 
+
+let name = "vivan"
+let age = 19
+let isLoggedIn = false
+
+
+//number
+// BigInt
+// String
+// Boolean
+//null => standalone value
+// undefined
+//symbol
+
+console.log(typeof null)
+console.log(typeof "vivan")
+console.log(typeof undefined)
