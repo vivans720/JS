c62a1370c7000104a90372664b0f211199161235
ritik236 <subhashrajput299a@gmai.com>
2023-09-28 22:51:54 +0530

    conversion and operations

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -16,4 +16,37 @@ let convertToNumber = Number(score);
 // let isLoggedIn = 0
 let isLoggedIn = 1;
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+let firstName = "vivan";
+let lastName = " sharma";
+
+// console.log(firstName + lastName);
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1,num2,num3 = 3;
+num1 = num2 = num3 = 10+5;
+
+let gameCounter = 100;
+// console.log(gameCounter++);
+// console.log(gameCounter);
+// console.log(++gameCounter);
+
